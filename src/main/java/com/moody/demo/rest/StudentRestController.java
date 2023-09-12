61da0bfa130c023d4daa9b74aea24680eb7fd7ba
Robert Moody <78385171+rlmoody@users.noreply.github.com>
2023-09-11 19:17:46 -0500

    Refactored code and added studentId path

--- src/main/java/com/moody/demo/rest/StudentRestController.java
+++ src/main/java/com/moody/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.moody.demo.rest;
 
 import com.moody.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+
+       theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Robert", "Moody"));
         theStudents.add(new Student("Daryl", "Hall"));
         theStudents.add(new Student("John", "Oates"));
 
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        // return list of students
         return theStudents;
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // index into list of students
+        return theStudents.get(studentId);
+    }
+
 }
